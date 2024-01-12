f7c46aed85716acf5b748665d3ad5d1179d133e9
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-01-12 15:14:05 +0200

    Login page functionality done
    Without SystemPage opening after it

--- LoginPage.java
+++ LoginPage.java
@@ -1,5 +1,7 @@
 package hospital_management_system;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -21,11 +23,39 @@ public class LoginPage implements ActionListener {
 	JLabel passwordLabel = new JLabel("Password:");
 	JLabel messageLabel = new JLabel();
 	
-	HashMap<String, String> loginInfoCopy = new HashMap<String, String>();
+	HashMap<String, String> loginInfo = new HashMap<String, String>();
 	
 	public LoginPage(HashMap<String, String> login_passwords){
 		
-		loginInfoCopy = login_passwords;
+		loginInfo = login_passwords;
+		
+		loginLabel.setBounds(50, 100, 75, 25);
+		passwordLabel.setBounds(50, 150, 75, 25);
+		
+		messageLabel.setBounds(140, 250, 250, 35);
+		messageLabel.setFont(new Font(null, Font.BOLD, 20));
+		 
+		loginField.setBounds(125, 100, 200, 25);
+		passwordField.setBounds(125, 150, 200, 25);
+		
+		loginButton.setBounds(125, 200, 100, 25);
+		loginButton.setFocusable(false);
+		loginButton.addActionListener(this);
+		
+		resetButton.setBounds(225, 200, 100, 25);
+		loginButton.setFocusable(false);
+		resetButton.addActionListener(this);
+		
+		frame.add(loginLabel);
+		frame.add(passwordLabel);
+		frame.add(messageLabel);
+		
+		frame.add(loginField);
+		frame.add(passwordField);
+		
+		frame.add(loginButton);
+		frame.add(resetButton);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(450, 450);
 		frame.setLayout(null);
@@ -34,7 +64,34 @@ public class LoginPage implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		
+		// reset button functionality
+		if(e.getSource()==resetButton) {
+			loginField.setText("");
+			passwordField.setText("");
+		}
+		
+		// login button functionality
+		if(e.getSource()==loginButton) {
+			String userLogin = loginField.getText();
+			String password = String.valueOf(passwordField.getPassword());
+			
+			if (loginInfo.containsKey(userLogin)) {
+				if (loginInfo.get(userLogin).equals(password)) {
+					messageLabel.setForeground(Color.green);
+					messageLabel.setText("You are logged in!");
+					
+					SystemPage systemPage = new SystemPage();
+				} else {
+					messageLabel.setForeground(Color.red);
+					messageLabel.setText("Wrong password!");
+				}
+			} else {
+				messageLabel.setForeground(Color.red);
+				messageLabel.setText("Login not found!");
+			}
+		}
 		
 	}
 
