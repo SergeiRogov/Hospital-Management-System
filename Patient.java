0a66876b1683f0d79f2f96c9ea6dbb2718b4ca61
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2024-01-14 04:02:02 +0200

    DOXYGEN COMMENTS

--- Patient.java
+++ Patient.java
@@ -1,16 +1,32 @@
+/**
+ * @file Patient.java
+ * @brief This file contains the Patient class.
+ */
 package hospital_management_system;
 
+/**
+ * @class Patient
+ * @brief Represents a hospital patient.
+ */
 public class Patient {
 	
+	// Patient attributes
 	private String name;
 	private String surname;
 	private String illness;
 	private String id;
-
 	private HospitalRoom room;
 	
+	// total number of patients ever created in a system
 	private static int patientCount = 0;
 
+	/**
+	 * @method Patient
+     * @brief Constructor of a patient.
+     * @param name Name of a patient.
+     * @param surname Surname of a patient.
+     * @param illness Illness of a patient.
+     */
     public Patient(String name, String surname, String illness) {
         this.name = name;
         this.surname = surname;
@@ -19,6 +35,15 @@ public class Patient {
         patientCount++;
     }
     
+    /**
+	 * @method Patient
+     * @brief Overloaded constructor of a patient.
+     * @param id ID of a patient.
+     * @param name Name of a patient.
+     * @param surname Surname of a patient.
+     * @param illness Illness of a patient.
+     * @param room Patient's room.
+     */
     public Patient(String id, String name, String surname, String illness, HospitalRoom room) {
     	this.id = id;
     	this.name = name;
@@ -27,27 +52,56 @@ public class Patient {
         this.room = room;
     }
     
+    /**
+	 * @method setRoom
+     * @brief Setter method for a room.
+     * @param room Patient's room.
+     */
     public void setRoom(HospitalRoom room) {
     	this.room = room;
     }
     
+    /**
+	 * @method getID
+     * @brief Getter method for ID.
+     * @return Patient's ID.
+     */
     public String getID() {
 		return id;
 	}
     
+    /**
+	 * @method getPatientCount
+     * @brief Getter method for patientCount.
+     * @return total number of patients ever created in a system.
+     */
     public static int getPatientCount() {
 		return patientCount;
 	}
     
+    /**
+	 * @method setPatientCount
+     * @brief Setter method for patientCount.
+     * @param count Total number of patients ever created in a system.
+     */
     public static void setPatientCount(int count) {
 		patientCount = count;
 	}
     
-    
+    /**
+	 * @method stringToFile
+     * @brief Converts a patient to string - to be stored in database file.
+     * return String representation.
+     */
     public String stringToFile() {
 		return id + ";" + name + ";" + surname + ";" + illness + ";" + room.getRoomID();
 	}
     
+    /**
+	 * @method toString
+     * @brief Converts a patient to string.
+     * return String representation.
+     */
     public String toString() {
 		return id + " [Name:" + name + " " + surname + "; Illness:" + illness + "]";
 	}
--- PatientList.java
+++ PatientList.java
@@ -1,28 +1,60 @@
+/**
+ * @file PatientList.java
+ * @brief This file contains the PatientList class.
+ */
 package hospital_management_system;
 
 import java.util.ArrayList;
 
+/**
+ * @class PatientList
+ * @brief Represents the list of patients.
+ */
 public class PatientList {
 	
+	// list of patients
 	private ArrayList<Patient> patients;
 	
+	/**
+	 * @method PatientList
+     * @brief Default constructor of a list of patients.
+     */
 	public PatientList() {
         this.patients = new ArrayList<>();
     }
 
+	/**
+	 * @method PatientList
+     * @brief Constructs a list of patients (overloaded constructor).
+     * @param patients List of hospital patients
+     */
 	public PatientList(ArrayList<Patient> patients) {
 		this.patients = patients;
 	}
 	
+	/**
+	 * @method addPatient
+	 * @param patients List of hospital patients
+     * @brief Adds a patient to a list.
+     */
 	public void addPatient(Patient patient) {
         patients.add(patient);
     }
 	
-	
+	/**
+	 * @method getPatientList
+     * @brief Getter method for a list of patients.
+     * @return List of patients.
+     */
 	public ArrayList<Patient> getPatientList() {
         return patients;
     }
 	
+	/**
+	 * @method removePatient
+     * @brief Removes given patient from a list.
+     * @param patient Hospital patient.
+     */
 	public void removePatient(Patient patient) {
 		patients.remove(patient);
 	}
